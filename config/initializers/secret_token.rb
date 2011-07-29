abab88c7d0573ca48ac7ca16c42cd08c4b5ba2cd
Bodacious <gavin@gavinmorrice.com>
2011-07-29 17:54:22 +0100

    Fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravisTest::Application.config.secret_token = '52351c47409f8eed8586432e247808a1059ce1472897c0b93a13551088a3cf7a98b635c206b8572ec4a1418fe51a39aec9341e19555ea82ee3a834f185b8f2de'
